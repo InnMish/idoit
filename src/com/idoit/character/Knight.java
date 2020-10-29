5e6caf0ee609945b9ed3a797967532484df6e86c
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2020-10-29 10:04:39 +0300

    Added solution for block1 lesson5

--- src/com/idoit/character/Knight.java
+++ src/com/idoit/character/Knight.java
@@ -1,6 +1,22 @@
 package com.idoit.character;
 
-public class Knight {
+import com.idoit.item.armor.*;
+import com.idoit.item.weapon.Sword;
 
+public class Knight {
+    public String name;
+    public int strength = 7;
+    public int agility;
+    public int intelligence;
     public int hp = 100;
+    public int mana = 100;
+    public int level = 1;
+    public int experience = 0;
+    public int gold = 100;
+    public Sword sword;
+    public Helmet helmet;
+    public Cuirass cuirass;
+    public Gloves gloves;
+    public Boots boots;
+    public Shield shield;
 }
