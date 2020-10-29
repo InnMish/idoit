337d8df67f3ce479889a96dffbd6a4fa7990ae08
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2020-10-29 09:22:02 +0300

    Added block1 lesson4 template

--- src/com/idoit/App.java
+++ src/com/idoit/App.java
@@ -3,7 +3,6 @@ package com.idoit;
 public class App {
 
     public static void main(String[] args) {
-        System.out.println("Vlad");
-        System.out.println(23);
+        Knight knight = new Knight();
     }
 }
---
+++ src/com/idoit/Knight.java
@@ -0,0 +1,5 @@
+package com.idoit;
+
+public class Knight {
+
+}
