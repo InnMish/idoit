767924cf313cd5330cf785487d3283a9cb28d818
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2020-10-30 14:18:37 +0300

    Added solution for block1 lesson9

--- src/com/idoit/character/Archer.java
+++ src/com/idoit/character/Archer.java
@@ -78,22 +78,138 @@ public class Archer {
     }
 
     public void setLeftRing(StrengthRing leftRing) {
+        agility += leftRing.getPointsToAdd(); //то же, что agility = agility + leftRing.getPointsToAdd();
         this.leftRing = leftRing;
     }
 
     public void setRightRing(StrengthRing rightRing) {
+        agility += rightRing.getPointsToAdd();
         this.rightRing = rightRing;
     }
 
     public void setBelt(StrengthBelt belt) {
+        agility += belt.getPointsToAdd();
         this.belt = belt;
     }
 
     public void setNecklace(StrengthNecklace necklace) {
+        agility += necklace.getPointsToAdd();
         this.necklace = necklace;
+    }
+
+    public void takeOffLeftRing() {
+        agility -= leftRing.getPointsToAdd(); // то же, что agility = agility - leftRing.getPointsToAdd();
+        leftRing = null;
+    }
+
+    public void takeOffRightRing() {
+        agility -= rightRing.getPointsToAdd();
+        rightRing = null;
+    }
+
+    public void takeOffBelt() {
+        agility -= belt.getPointsToAdd();
+        belt = null;
+    }
+
+    public void takeOffNecklace() {
+        agility -= necklace.getPointsToAdd();
+        necklace = null;
     }
 
     public void setActiveQuest(Quest activeQuest) {
         this.activeQuest = activeQuest;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHp() {
+        return hp;
+    }
+
+    public int getMana() {
+        return mana;
+    }
+
+    public int getStamina() {
+        return stamina;
+    }
+
+    public int getAgility() {
+        return agility;
+    }
+
+    public int getStrength() {
+        return strength;
+    }
+
+    public int getIntelligence() {
+        return intelligence;
+    }
+
+    public int getLevel() {
+        return level;
+    }
+
+    public int getExperience() {
+        return experience;
+    }
+
+    public int getGold() {
+        return gold;
+    }
+
+    public Bow getBow() {
+        return bow;
+    }
+
+    public Helmet getHelmet() {
+        return helmet;
+    }
+
+    public Cuirass getCuirass() {
+        return cuirass;
+    }
+
+    public Gloves getGloves() {
+        return gloves;
+    }
+
+    public Boots getBoots() {
+        return boots;
+    }
+
+    public Shield getShield() {
+        return shield;
+    }
+
+    public StrengthRing getLeftRing() {
+        return leftRing;
+    }
+
+    public StrengthRing getRightRing() {
+        return rightRing;
+    }
+
+    public StrengthBelt getBelt() {
+        return belt;
+    }
+
+    public StrengthNecklace getNecklace() {
+        return necklace;
+    }
+
+    public Point getPoint() {
+        return point;
+    }
+
+    public Quest getActiveQuest() {
+        return activeQuest;
+    }
+
+    public AccurateShot getSkill() {
+        return skill;
     }
 }
