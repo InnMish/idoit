a4628c757ca5fd82cbd9ef3da63174fe90efe63c
Vladislav Mishustin <vladislav.mishustin@onsolve.com>
2020-11-14 10:26:19 +0300

    block1 lesson8 Changed method modificators from private to public for tests

--- src/com/idoit/character/Knight.java
+++ src/com/idoit/character/Knight.java
@@ -68,11 +68,11 @@ public class Knight {
         this.experience = this.experience + exp;
     }
 
-    private void calculatePhysicalDefence() {
+    public void calculatePhysicalDefence() {
         physicalDefence = physicalDefence + helmet.physicalDefence + cuirass.physicalDefence + gloves.physicalDefence + boots.physicalDefence + shield.physicalDefence;
     }
 
-    private void calculateMagicDefence() {
+    public void calculateMagicDefence() {
         magicDefence = magicDefence + helmet.magicDefence + cuirass.magicDefence + gloves.magicDefence + boots.magicDefence + shield.magicDefence;
     }
 
